16853c0c6bd482c8a56e956795331ec081722624
Anuj-skills <atconnect000@gmail.com>
2025-12-13 21:18:56 +0530

    Delete Requirements.py

--- Requirements.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-import torch 
-import torch.nn as nn
-import torch.optim as optim
-import torchvision
-import torchvision.transforms as transforms
-
